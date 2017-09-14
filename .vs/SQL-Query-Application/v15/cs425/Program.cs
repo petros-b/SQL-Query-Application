aa24a5796e34c6fb378d09a3753730b521afd598
petros-b <pbesieri@umich.edu>
2017-09-14 14:47:38 -0400

    entered comment

---
+++ .vs/SQL-Query-Application/v15/cs425/Program.cs
@@ -0,0 +1,86 @@
+﻿/* Author: Petros Besieris
+ * Creation Date: 9/14/2017
+ * Modification Date: 9/15/2017
+ * Purpose: To allow user to enter in correct SQL commands and display queried results onto screen
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.ComponentModel;
+using System.Data.SqlClient;
+
+namespace cs425
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool choice = true;
+
+            while (choice == true)
+            {
+                Console.WriteLine("Enter in desired SQL Command:");
+                var input = "";
+
+                input = Console.ReadLine(); //read user's input (assuming SQL syntax is correct)
+
+                //test you bitch 
+
+                string con_string = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Petros\Documents\visual studio 2017\Projects\cs425\cs425\Database1.mdf; Integrated Security = True;";
+
+
+                exec_SQL(con_string, input); // execute user SQL command   
+
+                Console.WriteLine("Would you like to execute another SQL command?");
+               input = Console.ReadLine();
+                if (input == "yes" || input == "YES" || input == "Yes")
+                {
+                    choice = true;
+                }
+                else if (input == "NO" || input == "no" || input == "No")
+                {
+                    choice = false;
+                }
+
+            }
+
+            
+        }
+
+        static void exec_SQL(string connect_string, string sql_cmd)
+        {
+            SqlConnection connect = new SqlConnection(connect_string); // establish new connection with database 
+
+
+            SqlCommand comm = new SqlCommand(sql_cmd, connect); // initialize new SQL command with respect to the connected database
+
+            connect.Open(); // open connection of database 
+            SqlDataReader reader = comm.ExecuteReader(); //execute the SQL command
+
+            print_results(reader); // print results 
+
+
+            reader.Close(); // close connection once finished for security reasons
+        }
+
+
+        static void print_results(SqlDataReader r)
+        {
+            while (r.Read())
+            {
+                int counter = r.FieldCount; // set counter to the queried results 
+                for (int i = 0; i < counter; i++)
+                {
+                    Console.WriteLine(r.GetValue(i)); // print out queried results 
+                }
+            }
+
+        }
+    }
+}
+
+
